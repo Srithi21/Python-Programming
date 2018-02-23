5e46c2197757948fd2b7cecbc87f6fffe1c9643d
Srithi21 <36722913+Srithi21@users.noreply.github.com>
2018-02-23 10:14:56 +0530

    Create vowel or consonant.py

---
+++ vowel or consonant.py
@@ -0,0 +1,5 @@
+d=str(input())
+if((d=="a")or(d=="e")or(d=="i"))or(d=="o")or(d=="u")or(d=="A")or(d=="E")or(d=="I")or(d=="O")or(d=="U"):
+   print("The given character is a vowel" )
+else:
+   print("The given character is a consonant")
