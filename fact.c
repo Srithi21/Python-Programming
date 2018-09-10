5aa31239518d6ef8f44eeb88cb52b91cb1c4282f
Srithi21 <36722913+Srithi21@users.noreply.github.com>
2018-09-10 15:43:57 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    unsigned long long fact = 1;
+    scanf("%d",&n);
+    for(i=1; i<=n; ++i)
+        {
+            fact *= i;              
+        }
+    printf("%llu", fact);
+    
+
+    return 0;
+}
